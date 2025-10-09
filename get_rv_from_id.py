75a0f9bc006983ea4485f2b39f15a114d1a7f93b
Ava Holland <avaholland@Avas-MacBook-Air-2.local>
2025-10-09 11:19:14 -0400

    Test commit message

--- get_rv_from_id.py
+++ get_rv_from_id.py
@@ -3,5 +3,16 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 with fits.open("rcat_ngc6866_v0.fits") as hdu:
 	data = hdu[1].data
-	ids = data['GAIAEDR3_ID']
-	print(ids)
+	star_id = input("Enter a star ID:")
+	ids = data["GAIAEDR3_ID"]
+	rvs = data["vrad"]
+	dates = data["DATE-OBS"]
+	match = ids == star_id
+	star_rvs = rvs[match]
+	star_date = dates[match]
+	print(ids[match])
+	fig, ax = plt.subplots()
+	ax.scatter(star_date, star_rvs, s=10, c='blue', alpha=0.8,)
+	ax.set_xlabel("Time")
+	ax.set_ylabel("RV")
+	plt.show()
